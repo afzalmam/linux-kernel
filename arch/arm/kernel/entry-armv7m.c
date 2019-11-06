5283f5b70011b3e51286472ef909aaa1707c8317
afzal mohammed <afzal.mohd.ma@gmail.com>
2019-11-06 10:52:33 +0530

    [TMP] ARM: v7-M: pendsv entry: C of asm
    Signed-off-by: afzal mohammed <afzal.mohd.ma@gmail.com>

--- arch/arm/kernel/entry-armv7m.c
+++ arch/arm/kernel/entry-armv7m.c
@@ -6,11 +6,19 @@
 
 #include <linux/linkage.h>
 #include <linux/sched.h>
+#include <linux/context_tracking.h>
 #include <asm/v7m.h>
 #include <asm/ptrace.h>
 #include <asm/io.h>
+#include <asm/unistd.h>
 
 void nvic_handle_irq(irq_hw_number_t hwirq, struct pt_regs *regs);
+void addr_limit_check_failed(void);
+int do_work_pending(struct pt_regs *regs, unsigned int thread_flags,
+		    int syscall);
+#ifndef CONFIG_CONTEXT_TRACKING
+static inline void context_tracking_user_enter(void) {}
+#endif
 
 asmlinkage void _irq_entry(unsigned long unused, struct pt_regs *regs)
 {
@@ -30,4 +38,36 @@ asmlinkage void _irq_entry(unsigned long unused, struct pt_regs *regs)
 					BASEADDR_V7M_SCB + V7M_SCB_ICSR);
 		}
 	}
+}
+
+static inline void _reload_syscall_args(void) { }
+static inline void _local_restart(void) { }
+
+static void _ret_to_user_from_irq(struct pt_regs *reg, struct thread_info *tinfo,
+				  unsigned long scno)
+{
+	int ret;
+
+	if (current_thread_info()->addr_limit != TASK_SIZE)
+		addr_limit_check_failed();
+	if (current_thread_info()->flags & _TIF_WORK_MASK) {
+		ret = do_work_pending(reg, tinfo->flags, scno);
+		if (ret != 0) {
+			if (ret < 0)
+				scno = __NR_restart_syscall - __NR_SYSCALL_BASE;
+			_reload_syscall_args();
+			_local_restart();
+			return;
+		}
+	}
+	trace_hardirqs_on();
+	if (IS_ENABLED(CONFIG_CONTEXT_TRACKING))
+		context_tracking_user_enter();
+}
+
+asmlinkage void _pendsv_entry(struct pt_regs *reg)
+{
+	writel_relaxed(V7M_SCB_ICSR_PENDSVCLR,
+			BASEADDR_V7M_SCB + V7M_SCB_ICSR);
+	_ret_to_user_from_irq(reg, current_thread_info(), 0);
 }
